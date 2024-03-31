c95f00644d42c697f0f916ef783ba579876f7db0
vishnukoyyad03 <vishnukoyyad03@gmail.com>
2024-03-31 07:18:37 +0530

    added all the functionalities

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById('watchVideo1Btn').addEventListener('click', function() {
+    var videoId = '6foqGD24WqU'; // Replace with desired video ID
+    document.getElementById('videoFrame').src = "https://www.youtube.com/embed/" + videoId;
+  });
+  
+  document.getElementById('watchVideo2Btn').addEventListener('click', function() {
+    var videoId = '6foqGD24WqU'; // Replace with desired video ID
+    document.getElementById('videoFrame').src = "https://www.youtube.com/embed/" + videoId;
+  });
+  
